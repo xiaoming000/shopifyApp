15897382402119b1f532a090dd16ac0ba43b1c09
Realike <861579607@qq.com>
2019-08-14 14:37:28 +0800

    update

--- app/Models/variant.php
+++ app/Models/variant.php
@@ -10,6 +10,6 @@ class Variant extends Model
     protected $table = 'variant_test1';
 
     protected $fillable = ['id', 'sku', 'ali_item_id', 'shopify_variant_id', 'origin_price', 'seller_price', 'stock', 'title',
-        'image_url', 'ali_item_url', 'shopify_image_id', 'is_update', 'inventory_item_id', 'des_url' ];
+        'image_url', 'ali_item_url', 'shopify_image_id', 'is_update', 'inventory_item_id', 'shopify_product_id', 'shop_id', 'des_url'];
 
 }
